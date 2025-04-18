8e45efa7e795dff068eea6cbe779c83db9294504
Ron Feathers <ron.feathers@leandata.com>
2025-04-18 11:54:11 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../components/AuthProvider';
 import { useEffect } from 'react';
-// Add this to pages/_app.tsx after the import statements
-import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth } from '../components/AuthProvider';
 
@@ -20,6 +18,26 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
   }, [user, loading, router.pathname]);
 
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
+
+export default MyApp;
+
+// Server-side only import (will be excluded from client bundle)
+if (typeof window === 'undefined') {
+  // This will only run on the server
+  require('../lib/startup').runStartupValidations();
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Client-side initialization here if needed
+  }, []);
+  
   return (
     <AuthProvider>
       <Component {...pageProps} />
