f801e44c1f5b3e0c8c7c479d846e24345e7812c2
Ron Feathers <ron.feathers@leandata.com>
2025-04-29 11:39:09 -0700

    Update [id].tsx

--- pages/reviews/[id].tsx
+++ pages/reviews/[id].tsx
@@ -229,6 +229,13 @@ const ReviewPage: NextPage = () => {
       <span className="font-medium">OrgID:</span> {review.orgId}
       </div>
       )}
+     {/* Add the Kantata Project ID field */}
+    {review.kantataProjectId && (
+      <div>
+        <h3 className="text-sm font-medium text-gray-500">Kantata Project ID</h3>
+        <p className="mt-1">{review.kantataProjectId}</p>
+      </div>
+    )}
     {review.segment && (
       <div>
       <span className="font-medium">Segment:</span> {review.segment}
