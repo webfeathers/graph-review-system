f20b325a89d3c7828c0b180763cce9dd58e69c5a
Ron Feathers <ron.feathers@leandata.com>
2025-05-01 17:57:44 -0700

    Update [id].tsx

--- pages/reviews/[id].tsx
+++ pages/reviews/[id].tsx
@@ -84,7 +84,6 @@ const ReviewPage: NextPage = () => {
         },
         body: JSON.stringify({ status: newStatus })
       });
-      console.log(headers);
       
       if (!response.ok) {
         const errorData = await response.json();
